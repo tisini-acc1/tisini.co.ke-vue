c67d079b8a8364e8a49fc2bdfff822baa42aacf4
Felix Orinda <forinda82@gmail.com>
2023-06-15 16:02:07 +0300

    Refactor status and time count

--- src/components/OrgQuizCard.vue
+++ src/components/OrgQuizCard.vue
@@ -3,9 +3,8 @@ import { PropType, computed, ref, watchEffect } from "vue";
 import moment from "moment";
 import { QuestionSetInterface } from "@/types";
 import combineDateAndTime from "@/utils/combineDateAndTime";
-// import router from "@/router";
-// import { useRouter } from "vue-router";
-type PlayStatus = "in-progress" | "yet-to-play" | "played";
+import { qSetStatus } from "@/utils/QsetStatus";
+// Props
 const { qset, orgId } = defineProps({
   qset: {
     type: Object as PropType<QuestionSetInterface>,
@@ -16,21 +15,7 @@ const { qset, orgId } = defineProps({
     required: true,
   },
 });
-
-const playStatus = ref<PlayStatus>("yet-to-play");
-const getPlayStatus = (startDateTime: string, endDateTime: string) => {
-  const startTime = moment(startDateTime);
-  const endTime = moment(endDateTime);
-  if (moment().isBetween(startTime, endTime)) {
-    playStatus.value = "in-progress";
-  } else if (moment().isBefore(startTime)) {
-    playStatus.value = "yet-to-play";
-  } else if (endTime.isAfter(moment())) {
-    playStatus.value = "played";
-  }
-  return playStatus.value;
-};
-
+// Variables to be used in the template
 // This will hold the target time to countdown to
 const targetTime = ref(moment());
 
@@ -50,21 +35,21 @@ watchEffect(() => {
   // console.log(`Remaining time: ${remainingTime.value.asSeconds()} seconds`);
 });
 // Get time status and time left in hrs, mins and secs
-type timeStatus = "yet to play" | "played" | "playing";
-type timeLeft = {
+type TimeLeft = {
   hours: number | string;
   minutes: number | string;
   seconds: number | string;
 };
 
-type TimeStatusAndTimeLeft = {
-  timeStatus: timeStatus;
-  timeLeft: timeLeft;
-};
-const getTimeStatus = (): TimeStatusAndTimeLeft => {
-//   const endTime = combineDateAndTime(combineDateAndTime(qset.play_date,qset.start_time),combineDateAndTime(qset.play_date,qset.end_time));
-  const startTime = combineDateAndTime(qset.play_date, qset.start_time);
-  const rt = moment.duration(startTime.diff(moment()));
+
+const progressTime = (type: "start" | "end"): TimeLeft => {
+  //   const endTime = combineDateAndTime(combineDateAndTime(qset.play_date,qset.start_time),combineDateAndTime(qset.play_date,qset.end_time));
+  const startTime = moment(qSetStatus.getStartDate(qset));
+  const endTime = moment(qSetStatus.getEndDate(qset));
+  const rt = moment.duration(
+    type === "start" ? startTime.diff(moment()) : endTime.diff(moment())
+  );
+  // const rt = moment.duration(startTime.diff(moment()));
   const hrs = computed(() => {
     return Math.abs(rt.hours()) < 10
       ? String(Math.floor(Math.abs(rt.hours()))).padStart(2, "0")
@@ -87,149 +72,89 @@ const getTimeStatus = (): TimeStatusAndTimeLeft => {
   };
 
   if (remainingTime.value.asSeconds() < 0) {
-    return {
-      timeStatus: "played",
-      timeLeft,
-    };
+    return  timeLeft
+  
   }
   if (remainingTime.value.asSeconds() > 0) {
-    return {
-      timeStatus: "yet to play",
-      timeLeft,
-    };
+    return timeLeft
+
   }
-  return {
-    timeStatus: "playing",
-    timeLeft,
-  };
-};
-
-const getProgressStatus = (): timeLeft => {
-  const endTime = combineDateAndTime(combineDateAndTime(qset.play_date,qset.start_time).toLocaleString(),combineDateAndTime(qset.play_date,qset.end_time).toLocaleString());
-//   const startTime = combineDateAndTime(qset.play_date, qset.start_time);
-  const rt = moment.duration(moment().diff(endTime));
-  const hrs = computed(() => {
-    return Math.abs(rt.hours()) < 10
-      ? String(Math.floor(Math.abs(rt.hours()))).padStart(2, "0")
-      : Math.floor(Math.abs(rt.hours())).toFixed(0);
-  });
-  const mins = computed(() => {
-    return Math.abs(rt.minutes()) < 10
-      ? String(Math.abs(rt.minutes())).padStart(2, "0")
-      : Math.abs(rt.minutes()).toFixed(0);
-  });
-  const secs = computed(() => {
-    return Math.abs(rt.seconds()) < 10
-      ? String(Math.abs(rt.seconds())).padStart(2, "0")
-      : Math.abs(rt.seconds()).toFixed(0);
-  });
-  const timeLeft = {
-    hours: hrs.value,
-    minutes: mins.value,
-    seconds: secs.value,
-  };
-
-  // if (remainingTime.value.asSeconds() < 0) {
-  //   return {
-  //     timeStatus: "played",
-  //     timeLeft,
-  //   };
-  // }
-  // if (remainingTime.value.asSeconds() > 0) {
-  //   return {
-  //     timeStatus: "yet to play",
-  //     timeLeft,
-  //   };
-  // }
   return timeLeft
+  
 };
-// const router = useRouter();
-// const startQuiz = () => {
-//   console.log("start quiz");
-//   router.push(
-//     `/organizations/start-quiz?queryCategory=${qset.category_name}&qsetId=${
-//       qset.uid
-//     }&orgId=${orgId}&qStatus=${getPlayStatus(combineDateAndTime(qset.play_date,qset.start_time),combineDateAndTime(qset.play_date,qset.end_time))}`
-//   );
-// };
-// const startLink = computed(() => {
-//   return `/organizations/quiz-set/${qset.uid}?queryCategory=${
-//     qset.category_name
-//   }&orgId=${orgId}&qStatus=${getPlayStatus(combineDateAndTime(qset.play_date,qset.start_time),combineDateAndTime(qset.play_date,qset.end_time))}`;
-// });
+
 </script>
 
 <template>
   <li class="flex flex-col gap-1">
-    <h2 class="font-bold">{{ qset.category_name }}</h2>
-    <p class="font-medium text-blue-500">
-      Play date
-      {{
-        moment(combineDateAndTime(qset.play_date, qset.start_time)).format("LLLL")
-      }} {{getPlayStatus(combineDateAndTime(qset.play_date,qset.start_time).toString(),combineDateAndTime(qset.play_date,qset.end_time).toString())}}
-    </p>
-
-    <div>
-      <p
-        v-if="getPlayStatus(combineDateAndTime(qset.play_date,qset.start_time).toLocaleString(),combineDateAndTime(qset.play_date,qset.end_time).toLocaleString()) === 'in-progress'"
-      >
-        <span class="font-medium text-blue-500">Ends in -</span>
-        {{
-          `${getProgressStatus().hours}:${
-            getProgressStatus().minutes
-          }:${getProgressStatus().seconds}`
-        }}  
-    </p>
-      <p
-        class="font-medium text-blue-500"
-        v-if="getPlayStatus(combineDateAndTime(qset.play_date,qset.start_time).toLocaleString(),combineDateAndTime(qset.play_date,qset.end_time).toLocaleString()) === 'yet-to-play'"
-      >
-        {{
-          `${getTimeStatus().timeLeft.hours}:${
-            getTimeStatus().timeLeft.minutes
-          }:${getTimeStatus().timeLeft.seconds}`
-        }}
-        <div>
-          <router-link
-          type="button"
-          class="text-red-400 px-2 py-1 rounded border"
-          :to="{name:'organization-quizsets', query:{queryCategory:qset.category_name, orgId:orgId, qStatus:getPlayStatus(combineDateAndTime(qset.play_date,qset.start_time).toLocaleString(),combineDateAndTime(qset.play_date,qset.end_time).toLocaleString())}, params:{qsetId:qset.uid}}"
+    <div class="flex justify-between border-b py-2">
+      <h1 class="font-bold">{{ qset.category_name }}</h1>
+      <!-- Badge -->
+      <div>
+        <span
+          v-if="qSetStatus.getStatus(qset) === 'not-started'"
+          :class="[qSetStatus.getBadgeColor(qset)]"
         >
-          Play now
-        </router-link>
-        <button type="button" class="text-red-400 px-2 py-1 rounded border">
-          {{
-            moment(combineDateAndTime(combineDateAndTime(qset.play_date,qset.start_time).toLocaleString(),combineDateAndTime(qset.play_date,qset.end_time).toLocaleString())).fromNow()
-          }}
-        </button>
-        </div>
+          Not started
+        </span>
+        <span
+          v-if="qSetStatus.getStatus(qset) === 'in-progress'"
+          :class="[qSetStatus.getBadgeColor(qset)]"
+        >
+          In progress
+        </span>
+        <span
+          v-if="qSetStatus.getStatus(qset) === 'closed'"
+          :class="[qSetStatus.getBadgeColor(qset)]"
+        >
+          Closed
+        </span>
+      </div>
+    </div>
+    <div
+      v-if="qSetStatus.getStatus(qset) === 'not-started'"
+      class="flex flex-col gap-1"
+    >
+      <div></div>
+      <p class="text-center">
+        <span class=""> Will open in</span>
+        <h1 class="tracking-wider font-mono font-bold text-lg">
+          {{ progressTime("start").hours }}:{{
+          progressTime("start").minutes
+        }}:{{ progressTime("start").seconds }}
+        </h1>
+       
       </p>
-      <div
-        class="font-medium text-blue-500 grid grid-cols-2 gap-2 text-sm"
-        v-else-if="getPlayStatus(combineDateAndTime(qset.play_date,qset.start_time).toLocaleString(),combineDateAndTime(qset.play_date,qset.end_time).toLocaleString()) === 'in-progress'"
-      >
-      <router-link
+    </div>
+    <div
+      v-if="qSetStatus.getStatus(qset) === 'in-progress'"
+      class="flex flex-col gap-1"
+    >
+    <p class="text-center">
+      <span class=""> Will close in</span>
+      <h1 class="tracking-wider font-mono font-bold text-lg">
+        {{ progressTime("end").hours }}:{{
+          progressTime("end").minutes
+        }}:{{ progressTime("end").seconds }}
+        </h1>
+        
+      </p>
+      <div>
+        <router-link
           type="button"
-          class="text-red-400 px-2 py-1 rounded border"
-          :to="{name:'organization-quizsets', query:{queryCategory:qset.category_name, orgId:orgId, qStatus:getPlayStatus(combineDateAndTime(qset.play_date,qset.start_time).toLocaleString(),combineDateAndTime(qset.play_date,qset.end_time).toLocaleString())}, params:{qsetId:qset.uid}}"
+          class="text-white bg-green-800 font-medium uppercase w-full text-center px-2 py-1 rounded border"
+          :to="{name:'organization-quizsets', query:{queryCategory:qset.category_name, orgId:orgId, qStatus:qSetStatus.getStatus(qset)}, params:{qsetId:qset.uid}}"
         >
-          Play now
+          Start Quiz
         </router-link>
-        <button type="button" class="text-red-400 px-2 py-1 rounded border">
-          {{
-           `${getProgressStatus().hours}:${
-            getProgressStatus().minutes}`
-          }}
-        </button>
       </div>
-      <div
-        class="font-medium text-red-500"
-        v-else
-      >
-        
-        Ends
-        
-      </div>
+      
+    </div>
+    <div
+      v-if="qSetStatus.getStatus(qset) === 'closed'"
+      class="flex flex-col gap-1"
+    >
+      <h1 class="text-red-500 font-bold text-center py-4 uppercase">Quiz Closed</h1>
     </div>
   </li>
 </template>
