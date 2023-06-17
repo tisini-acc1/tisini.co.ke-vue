0504c9963a86903cd1c7c68740b8059ae65d244e
Felix Orinda <forinda82@gmail.com>
2023-06-17 16:00:27 +0300

    Add dummy articles

---
+++ src/data/articles.tsx
@@ -0,0 +1,281 @@
+import { ArticleInterface } from "@/types";
+
+const articles: ArticleInterface[] = [
+  {
+    article_title: "Article Title",
+    featured_image: "https://picsum.photos/id/7/600/600",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    article_body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    id: 1,
+    author: {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      author_email: "some@gmail.com",
+    },
+    publish: "2021-09-09T12:00:00.000000Z",
+    tags: ["tag1", "tag2"],
+    is_editors_pick: true,
+    is_featured: true,
+    is_sponsored: true,
+    slug: "article-title",
+    article_category: {
+      id: 1,
+      article_category: "category1",
+    },
+  },
+  {
+    article_title: "Article Title",
+    featured_image: "https://picsum.photos/id/7/600/600",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    article_body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    id: 1,
+    author: {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      author_email: "some@gmail.com",
+    },
+    publish: "2021-09-09T12:00:00.000000Z",
+    tags: ["tag1", "tag2"],
+    is_editors_pick: true,
+    is_featured: true,
+    is_sponsored: true,
+    slug: "article-title",
+    article_category: {
+      id: 1,
+      article_category: "category1",
+    },
+  },
+  {
+    article_title: "Article Title",
+    featured_image: "https://picsum.photos/id/7/600/600",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    article_body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    id: 1,
+    author: {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      author_email: "some@gmail.com",
+    },
+    publish: "2021-09-09T12:00:00.000000Z",
+    tags: ["tag1", "tag2"],
+    is_editors_pick: true,
+    is_featured: true,
+    is_sponsored: true,
+    slug: "article-title",
+    article_category: {
+      id: 1,
+      article_category: "category1",
+    },
+  },
+  {
+    article_title: "Article Title",
+    featured_image: "https://picsum.photos/id/7/600/600",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    article_body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    id: 1,
+    author: {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      author_email: "some@gmail.com",
+    },
+    publish: "2021-09-09T12:00:00.000000Z",
+    tags: ["tag1", "tag2"],
+    is_editors_pick: true,
+    is_featured: true,
+    is_sponsored: true,
+    slug: "article-title",
+    article_category: {
+      id: 1,
+      article_category: "category1",
+    },
+  },
+  {
+    article_title: "Article Title",
+    featured_image: "https://picsum.photos/id/7/600/600",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    article_body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    id: 1,
+    author: {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      author_email: "some@gmail.com",
+    },
+    publish: "2021-09-09T12:00:00.000000Z",
+    tags: ["tag1", "tag2"],
+    is_editors_pick: true,
+    is_featured: true,
+    is_sponsored: true,
+    slug: "article-title",
+    article_category: {
+      id: 1,
+      article_category: "category1",
+    },
+  },
+  {
+    article_title: "Article Title",
+    featured_image: "https://picsum.photos/id/7/600/600",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    article_body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    id: 1,
+    author: {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      author_email: "some@gmail.com",
+    },
+    publish: "2021-09-09T12:00:00.000000Z",
+    tags: ["tag1", "tag2"],
+    is_editors_pick: true,
+    is_featured: true,
+    is_sponsored: true,
+    slug: "article-title",
+    article_category: {
+      id: 1,
+      article_category: "category1",
+    },
+  },
+  {
+    article_title: "Article Title",
+    featured_image: "https://picsum.photos/id/7/600/600",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    article_body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    id: 1,
+    author: {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      author_email: "some@gmail.com",
+    },
+    publish: "2021-09-09T12:00:00.000000Z",
+    tags: ["tag1", "tag2"],
+    is_editors_pick: true,
+    is_featured: true,
+    is_sponsored: true,
+    slug: "article-title",
+    article_category: {
+      id: 1,
+      article_category: "category1",
+    },
+  },
+  {
+    article_title: "Article Title",
+    featured_image: "https://picsum.photos/id/7/600/600",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    article_body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    id: 1,
+    author: {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      author_email: "some@gmail.com",
+    },
+    publish: "2021-09-09T12:00:00.000000Z",
+    tags: ["tag1", "tag2"],
+    is_editors_pick: true,
+    is_featured: true,
+    is_sponsored: true,
+    slug: "article-title",
+    article_category: {
+      id: 1,
+      article_category: "category1",
+    },
+  },
+  {
+    article_title: "Article Title",
+    featured_image: "https://picsum.photos/id/7/600/600",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    article_body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    id: 1,
+    author: {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      author_email: "some@gmail.com",
+    },
+    publish: "2021-09-09T12:00:00.000000Z",
+    tags: ["tag1", "tag2"],
+    is_editors_pick: true,
+    is_featured: true,
+    is_sponsored: true,
+    slug: "article-title",
+    article_category: {
+      id: 1,
+      article_category: "category1",
+    },
+  },
+  {
+    article_title: "Article Title",
+    featured_image: "https://picsum.photos/id/7/600/600",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    article_body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    id: 1,
+    author: {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      author_email: "some@gmail.com",
+    },
+    publish: "2021-09-09T12:00:00.000000Z",
+    tags: ["tag1", "tag2"],
+    is_editors_pick: true,
+    is_featured: true,
+    is_sponsored: true,
+    slug: "article-title",
+    article_category: {
+      id: 1,
+      article_category: "category1",
+    },
+  },
+  {
+    article_title: "Article Title",
+    featured_image: "https://picsum.photos/id/7/600/600",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    article_body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    id: 1,
+    author: {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      author_email: "some@gmail.com",
+    },
+    publish: "2021-09-09T12:00:00.000000Z",
+    tags: ["tag1", "tag2"],
+    is_editors_pick: true,
+    is_featured: true,
+    is_sponsored: true,
+    slug: "article-title",
+    article_category: {
+      id: 1,
+      article_category: "category1",
+    },
+  },
+];
+
+export default articles;
